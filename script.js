1c6c8485b303b649c8e062f9e7a22a0e22d7f39b
“MarkGregory1 git config --global user.email “markgregory016@gmail.com <student@Cumberlandss-MacBook-Pro-10.local>
2022-03-03 00:23:31 -0500

    LiteralMathMethodExercise

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+// 1b
+console.log(warmHugs.toUpperCase());
+// 1c
+console.log(warmHugs.replace("like" , "love"));
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled.";
+// 2b
+console.log(beenImpaled.slice(-18));
+// 3a
+let dotDotDot = "...";
+// 3b
+let skullBones = `I don't have a skull${dotDotDot}or bones.`;
+console.log(skullBones);
+// 4
+console.log(Math.PI);
+// 5
+console.log(randomNumber = Math.floor(Math.random() * 3) + 1);
+// let randomNumber = Math.random();
+// randomNumber *= 3;
+// randomNumber ++;
+// randomNumber = Math.floor(randomNumber);
+// randomNumber = Math.ceil(randomNumber);
+
+
+// BONUS
+// 6
+console.log(" Let It Go!".toUpperCase().repeat(2).slice(-21));
+// 7a
+let reindeers = "Reindeers are better than people"
+// 7b
+console.log(reindeers.replace("Reindeers are better than people","Reindeers_Are_Better_Than_People"));
+// 8
+let squareRoot = Math.sqrt(2);
+console.log(squareRoot);
+// 9
+console.log(newRandomNumber = Math.floor(Math.random() * 17) + 7);
+// the floor rounds down(=7) (+17=23.999)also rounds down?
